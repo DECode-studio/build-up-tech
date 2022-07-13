2f5fc0720cf10361e9d62acd5b4b284face84c0c
DECode.studio <decode.std@gmail.com>
2022-07-13 12:43:27 +0700

    add new data

--- pages/dashboard/components/about_section.php
+++ pages/dashboard/components/about_section.php
@@ -6,16 +6,19 @@
 
             <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
                 <div class="content">
-                    <h3>Kenapa STMJ ?</h3>
-                    <h2 style="text-justify: newspaper;">Soul Tracking Mobile Kunction (STMJ) adalah solusi tepat dalam pemantauan penjelajah di Alam</h2>
+                    <h3>Mengapa Build Up Tech ?</h3>
+                    <h2 style="text-justify: newspaper;">
+                        Build Up Tech adalah solusi tepat dalam pemilihan jasa pengembang bidang teknologi.
+                    </h2>
                     <p style="text-align: justify !important;">
-                        STMJ merupakan teknologi yang dapat memantau koordinat dan kondisi pendaki di Alam. Banyaknya peminat penjelajahan berbanding lurus dengan tingginya resiko kecelakaan yang dapat terjadi di Alam. Oleh sebab itu dibutuhkan suatu teknologi yang dapat memantau lokasi dan kondisi penjelajah di Alam, agar dapat mengurangi resiko kecelakaan tersebut. Namun, kurangnya konektifitas internet yang ada di Gunung menjadi kendala akan solusi tersebut. STMJ dapat menjadi solusi untuk pemantauan lokasi dan kondisi penjelajah, guna meminimalisir tingginya resiko kecelakaan, dikarenakan STMJ dapat mentransmisikan data lokasi dan kondisi penjelajah di Alam menggunakan gelombang radio yang tidak berpengaruh pada konektifitas Internet.
+                        Semenjak masuknya era 4.0, kemajuan teknologi tidak dapat dibendung, bahkan setaip aspek pada kehidupan bergantung pada teknologi. Kemajuan teknologi memberikan dampak positif terhadap kemajuan pengetahuan, infrastruktur, pengembangan bisnis dan pola hidup masyarakat.
+                        Build Up Tech merupakan pengembang jasa teknologi yang menawarkan solusi moderen berbasis teknologi dalam mengatasi isu - isu permasalahan yang ada terkait teknologi. Build Up Tech menggunakan teknologi - teknologi modern yang sedang populer dan terkenal dalam mengembangkan teknologinya, sehingga dapat bersaing dalam panasnya kemajuan teknologi.
                     </p>
                 </div>
             </div>
 
             <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
-                <img src="assets/img/traveller.jpg" class="img-fluid" alt="">
+                <img src="../../../assets/vector/bg_modern.png" class="img-fluid" alt="">
             </div>
 
         </div>
